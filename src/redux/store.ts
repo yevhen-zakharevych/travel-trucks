00b98d690130155984b083fac8501d1c017e0ed7
Yevhen Zakharevych <yzakharevych@fieldcomplete.com>
2024-09-15 20:25:14 +0200

    fix store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -43,19 +43,5 @@ export const store = configureStore({
 
 export const persistor = persistStore(store);
 
-// import { configureStore } from '@reduxjs/toolkit';
-
-// import filtersReducer from './filtersSlice';
-// import campersReducer from './campersSlice';
-// import favoritesReducer from './favoritesSlice';
-
-// export const store = configureStore({
-//   reducer: {
-//     filters: filtersReducer,
-//     campers: campersReducer,
-//     favorites: favoritesReducer,
-//   },
-// });
-
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
